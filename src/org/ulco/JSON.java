559b5522e7aeb3b2515991d66d7d4c3e1a07383f
Nicolas <nicolas.pestelle@hotmail.fr>
2017-11-20 11:03:36 +0100

    histoire2 finis

--- src/org/ulco/JSON.java
+++ src/org/ulco/JSON.java
@@ -1,18 +1,31 @@
 package org.ulco;
 
+import java.lang.reflect.Constructor;
+
 public class JSON {
     static public GraphicsObject parse(String json) {
         GraphicsObject o = null;
         String str = json.replaceAll("\\s+", "");
         String type = str.substring(str.indexOf("type") + 5, str.indexOf(","));
 
-        if (type.compareTo("square") == 0) {
+       /* if (type.compareTo("square") == 0) {
             o = new Square(str);
         } else if (type.compareTo("rectangle") == 0) {
             o = new Rectangle(str);
         } else if (type.compareTo("circle") == 0) {
             o = new Circle(str);
+        }*/
+
+        String nomClass = type.substring(0, 1).toUpperCase() + type.substring(1);
+
+        try {
+            Class classe = Class.forName("org.ulco."+nomClass);
+            Constructor constructeur = classe.getConstructor(String.class);
+            o = (GraphicsObject)constructeur.newInstance(new String[]{str});
+        } catch (Exception e) {
+            e.getMessage();
         }
+
         return o;
     }
 
