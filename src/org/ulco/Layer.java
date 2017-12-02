f27a10b57f57d8aea777c904021443a3f9194c11
Pestelle <nicolas.pestelle@hotmail.fr>
2017-12-02 22:41:32 +0100

    histoire 5 faite

--- src/org/ulco/Layer.java
+++ src/org/ulco/Layer.java
@@ -77,7 +77,7 @@ public class Layer {
         }
     }
 
-    public GraphicsObjects select(Point pt, double distance) {
+   /* public GraphicsObjects select(Point pt, double distance) {
         GraphicsObjects list = new GraphicsObjects();
 
         for (GraphicsObject object : m_list) {
@@ -86,6 +86,10 @@ public class Layer {
             }
         }
         return list;
+    }*/
+
+    public Vector<GraphicsObject> getM_list() {
+        return m_list;
     }
 
     public String toJson() {
